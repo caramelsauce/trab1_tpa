f630853e6aafea8ef08cd0a26e32316a41ac2171
Caramel Sauce <tyleralderson97@gmail.com>
2020-02-16 12:23:56 -0300

    Inserção ordenada por nome e codigo

--- lista.c
+++ lista.c
@@ -20,37 +20,96 @@ void inserir(struct lista *listaNome, struct lista *listaCodigo, char *nome,
     struct cidade *nova_cidade = malloc(sizeof(struct cidade));
     strcpy(nova_cidade->nome, nome);
     nova_cidade->codigo = codigo;
-    struct no *elemento = malloc(sizeof(struct no));
-    elemento->p_cidade = nova_cidade;
-    elemento->prox = elemento->ant = NULL;
-    //insereNome(listaNome, elemento);
-    insereCodigo(listaCodigo, elemento);
+    struct no *elemento_nome = malloc(sizeof(struct no));
+    struct no *elemento_codigo = malloc(sizeof(struct no));
+    elemento_nome->p_cidade = nova_cidade;
+    elemento_nome->prox = elemento_nome->ant = NULL;
+    elemento_codigo->p_cidade = nova_cidade;
+    elemento_codigo->prox = elemento_codigo->ant = NULL;
+    insereNome(listaNome, elemento_nome);
+    insereCodigo(listaCodigo, elemento_codigo);
+}
+
+void insereNome(struct lista *lista, struct no *elemento)
+{
+    if (listaVazia(lista))
+        lista->prim = lista->ult = elemento;
+    else
+    {
+        struct no *aux = lista->prim;
+        while (aux != NULL && strcmp(elemento->p_cidade->nome, aux->p_cidade->nome) > 0)
+        {
+            aux = aux->prox;
+        }
+        if (aux == NULL)
+        {
+            lista->ult->prox = elemento;
+            elemento->ant = lista->ult;
+            lista->ult = elemento;
+        }
+        else if (aux->ant == NULL)
+        {
+            elemento->prox = aux;
+            aux->ant = elemento;
+            lista->prim = elemento;
+        }
+        else
+        {
+            aux->ant->prox = elemento;
+            elemento->ant = aux->ant;
+            aux->ant = elemento;
+            elemento->prox = aux;
+        }
+    }
 }
 
 void insereCodigo(struct lista *lista, struct no *elemento)
 {
-    if (listaVazia(lista) || lista->prim->p_cidade->codigo >= elemento->p_cidade->codigo)
-    {
-        elemento->prox = lista->prim;
-        lista->prim = elemento;
-    }
+    if (listaVazia(lista))
+        lista->prim = lista->ult = elemento;
     else
     {
         struct no *aux = lista->prim;
-        while (aux->prox != NULL && aux->prox->p_cidade->codigo < elemento->p_cidade->codigo)
+        while (aux != NULL && elemento->p_cidade->codigo > aux->p_cidade->codigo)
         {
             aux = aux->prox;
         }
-        elemento->prox = aux->prox;
-        elemento->ant = aux;
-        aux->prox = elemento;
+        if (aux == NULL)
+        {
+            lista->ult->prox = elemento;
+            elemento->ant = lista->ult;
+            lista->ult = elemento;
+        }
+        else if (aux->ant == NULL)
+        {
+            elemento->prox = aux;
+            aux->ant = elemento;
+            lista->prim = elemento;
+        }
+        else
+        {
+            aux->ant->prox = elemento;
+            elemento->ant = aux->ant;
+            aux->ant = elemento;
+            elemento->prox = aux;
+        }
     }
 }
 
 void exibir(struct lista *listaNome, struct lista *listaCodigo)
 {
     int i = 1;
-    struct no *aux = listaCodigo->prim;
+    struct no *aux = listaNome->prim;
+    printf("\nLista ordenada por nome:\n");
+    while (aux != NULL)
+    {
+        printf("%d %s %d\n", i, aux->p_cidade->nome, aux->p_cidade->codigo);
+        aux = aux->prox;
+        i++;
+    }
+    aux = listaCodigo->prim;
+    i = 1;
+    printf("\nLista ordenada por código:\n");
     while (aux != NULL)
     {
         printf("%d %s %d\n", i, aux->p_cidade->nome, aux->p_cidade->codigo);
--- main.c
+++ main.c
@@ -5,11 +5,14 @@
 int main(int argc, char *argv[])
 {
     struct lista listaCodigo;
+    struct lista listaNome;
     inicLista(&listaCodigo);
-    inserir(NULL, &listaCodigo, "Vitória", 4321);
-    inserir(NULL, &listaCodigo, "Colatina", 2222);
-    inserir(NULL, &listaCodigo, "Belo Horizonte", 1234);
-    exibir(NULL, &listaCodigo);
-    printf("%s", listaCodigo.prim->prox->p_cidade->nome);
+    inicLista(&listaNome);
+    inserir(&listaNome, &listaCodigo, "Vitória", 4321);
+    inserir(&listaNome, &listaCodigo, "Colatina", 2222);
+    inserir(&listaNome, &listaCodigo, "Belo Horizonte", 1234);
+    inserir(&listaNome, &listaCodigo, "Sao Paulo", 9999);
+    exibir(&listaNome, &listaCodigo);
+    //printf("%s", listaCodigo.prim->prox->p_cidade->nome);
     return 0;
 }
