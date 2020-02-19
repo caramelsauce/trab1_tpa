ae365741c31245750fdad37596b2f6b1e2381ca1
Caramel Sauce <tyleralderson97@gmail.com>
2020-02-19 20:34:55 -0300

    feito a base do menu de interação com o programa

--- main.c
+++ main.c
@@ -3,21 +3,39 @@
 #include "lista.h"
 #include "arquivo.h"
 
+void menu()
+{
+    printf("\n\n");
+    printf("1 - Efetuar busca por nome\n");
+    printf("2 - Efetuar busca por código\n");
+    printf("3 - Sair\n");
+    printf("Opção: ");
+}
+
 int main(int argc, char *argv[])
 {
+    int quit = 0, op;
     struct lista listaCodigo;
     struct lista listaNome;
     inicLista(&listaCodigo);
     inicLista(&listaNome);
-    /*inserir(&listaNome, &listaCodigo, "Vitória", 4321);
-    inserir(&listaNome, &listaCodigo, "Colatina", 2222);
-    inserir(&listaNome, &listaCodigo, "Belo Horizonte", 1234);
-    inserir(&listaNome, &listaCodigo, "Sao Paulo", 9999);
-    exibir(&listaNome, &listaCodigo);
-    consultarCodigo(&listaNome, 1234);
-    */
+    printf("Carregando dados do arquivo de texto...\n");
     preencherListas(&listaNome, &listaCodigo, "entrada.txt");
-    exibir(&listaNome, &listaCodigo);
+    while (!quit)
+    {
+        menu();
+        scanf("%d", &op);
+        switch (op)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            quit = 1;
+            break;
+        }
+    }
     gerarArquivos(&listaNome, &listaCodigo);
     return 0;
 }
